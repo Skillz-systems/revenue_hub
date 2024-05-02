75b9f7109f071d4d2c64021893c98056574de2d5
Ahire Tersoo David <tersooahire21@gmail.com>
2024-05-02 13:58:44 +0100

    update

--- Frontend/src/Components/project-layout/ProjectLayout.jsx
+++ Frontend/src/Components/project-layout/ProjectLayout.jsx
@@ -1,11 +1,14 @@
-import SearchInput from "../Searchinput/SearchInput";
+import SideBarMenu from "../../side-bar-menu/SideBarMenu";
+import SearchInput from "../SearchInput/SearchInput";
 
 export default function ProjectLayout() {
   return (
     <div className="flex h-screen gap-4 bg-custom-blue-100 lg:p-4">
-      <div className="max-w-60">LEFT</div>
+      <div className="max-w-60">
+        <SideBarMenu />
+      </div>
       <div className="w-full bg-white border-0.6 border-custom-border rounded">
-        <SearchInput/>
+        {/* <SearchInput /> */}
         RIGHT
       </div>
     </div>
