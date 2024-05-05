a20a81682caa2db8bfa1d0760bbf62d385a5cad1
Highpriest26 <nwodochukwuemeka26@.com>
2024-05-05 04:50:37 -0700

    resolve-conflict

---
+++ Frontend/src/Components/CardComponents/PropertyCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+function PropertyCard () {
+    return (
+        <div>
+           <div>
+            <div></div>
+           </div>
+        </div>
+    ) 
+}
+
+
+
+export default PropertyCard;
