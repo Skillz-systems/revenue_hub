968e2447fb671151068839667061fa8bf13f915c
Highpriest26 <nwodochukwuemeka26@.com>
2024-05-05 08:21:10 -0700

    Resolve-merge-conflict

--- Frontend/src/Components/Index.js
+++ Frontend/src/Components/Index.js
@@ -3,5 +3,5 @@ export { default as MenuItem } from './MenuItem/MenuItem';
 export { default as ProfileBox } from './ProfileBox/ProfileBox';
 export { default as SearchInput } from './SearchInput/SearchInput';
 export { default as MenuItemData } from './MenuItem/MenuItemData';
-
+export { default as Card } from './Card/Card';
 
--- Frontend/src/Components/ProjectLayout/ProjectLayout.jsx
+++ Frontend/src/Components/ProjectLayout/ProjectLayout.jsx
@@ -3,13 +3,11 @@ import MenuItemAlt from "../MenuItem/MenuItemAlt";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FiSearch } from "react-icons/fi";
 import MenuItemData from "../MenuItem/MenuItemData";
+import {Card} from "../Index";
 import SideBarMenu from "../../Pages/SideBarMenu/SideBarMenu";
-<<<<<<< HEAD
 import images from '../../assets/index';
-=======
 import TopNavigation from "../TopNavigation/TopNavigation";
-import Card from "../CardComponents/Card";
->>>>>>> 30d915dfbbd1d59185e62a05eaa246e2ab2d8744
+
 
 export default function ProjectLayout() {
   const [displaySideBarMenu, setdisplaySideBarMenu] = useState(true);
@@ -105,42 +103,43 @@ export default function ProjectLayout() {
         )}
       </div>
       <div
-        className={`flex-col bg-white border-0.6 border-custom-border rounded ${
+        className={`flex-col p-4 bg-white border-0.6 border-custom-border rounded ${
           transitionSection
             ? "w-full transition-all ease-in-out duration-500"
             : "w-5/6"
         }`}
       >
-<<<<<<< HEAD
-        <Card
+        <div className="grid grid-cols-2 gap-4">
+          <div><Card
         icon={<img src={images.Shieldblue} alt="Icon" />}
-        height="25%"
-        width="100%"
         title="Value of Generated"
         subtitle="Demand notice"
         value="4,400,000.00"
         withSymbol= {true}
         datepicker={true}
         dateContent="April, 2024"
-      />
+      /></div>
+          <div><Card
+        icon={<img src={images.Shieldblue} alt="Icon" />}
+        title="Value of Generated"
+        subtitle="Demand notice"
+        value="4,400,000.00"
+        style='bg-white'
+        dateContent="April, 2024"
+      /></div>
+          <div><Card
+        icon={<img src={images.Shieldblue} alt="Icon" />}
+        title="Value of Generated"
+        subtitle="Demand notice"
+        value="4,400,000.00"
+        withSymbol= {true}
+        datepicker={true}
+        dateContent="April, 2024"
+      /></div>
+        </div>
+        
 
         RIGHT
-=======
-        <TopNavigation
-          userName={"John"}
-          handleMenuClick={() => {
-            alert("Opened Menu Modal");
-          }}
-        />
-        {/* <Card
-        icon={<img src="path/to/icon.png" alt="Icon" />}
-        height="25%"
-        width="100%"
-        title="Custom Title"
-        subtitle="Custom Subtitle"
-        value="Custom Value"
-      /> */}
->>>>>>> 30d915dfbbd1d59185e62a05eaa246e2ab2d8744
       </div>
     </div>
   );
