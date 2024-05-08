4e33806f1a37178ad50d9d0254f009fa38f92578
Ahire Tersoo David <tersooahire21@gmail.com>
2024-05-08 19:32:49 +0100

    c

--- Frontend/src/Components/Card/Card.jsx
+++ Frontend/src/Components/Card/Card.jsx
@@ -13,12 +13,12 @@ const Card = ({ icon, title, subtitle, value, style, withSymbol = false, datepic
 
                     <div className="flex flex-col justify-center ">
                         <h3 className='text-gray-400 font-lexend font-[400] text-[12px] leading-[14px]'>{title}</h3>
-                        <h1 className='text-stone-950 font-bold font-lexend font-[600] text[14px] leading-[16px]'>{subtitle}</h1>
+                        <h1 className='text-stone-950 font-bold font-lexend text[14px] leading-[16px]'>{subtitle}</h1>
                     </div>
                 </div>
                 <div>
-                    {datepicker && (<div className='flex space-between h-fit w-fit items-center border rounded-md border border-[#CCD0DC] p-1 space-x-1 bg-[#F6F8FA]'>
-                        <div className='font-lexend font-medium text-gray-400 text-base tracking-wide'>{dateContent}</div>
+                    {datepicker && (<div className='flex space-between h-fit w-fit items-center border rounded-md border-[#CCD0DC] p-1 space-x-1 bg-[#F6F8FA]'>
+                        <div className='text-base font-medium tracking-wide text-gray-400 font-lexend'>{dateContent}</div>
                         <div className='h-3 w-[1px] bg-[#CCD0DC]'></div>
                         <img src={images.DateIcon} alt='dateIcon' className='w-4 h-4' />
                     </div>)}
