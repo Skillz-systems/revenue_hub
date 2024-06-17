09ce1d9abc0214b8a9a823b42729c1f9fe60c9cc
unknown <Samuel Kayode>
2024-06-17 09:28:00 +0100

    crud for group

--- Backend/app/Http/Controllers/GroupController.php
+++ Backend/app/Http/Controllers/GroupController.php
@@ -1,65 +1,514 @@
 <?php
+<?php
 
 namespace App\Http\Controllers;
 
 use App\Models\Group;
 use Illuminate\Http\Request;
+use App\Service\GroupService;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\GroupResource;
+use Illuminate\Support\Facades\Validator;
 
 class GroupController extends Controller
 {
+    protected $groupService;
+
+    public function __construct(GroupService $groupService)
+    {
+        $this->groupService = $groupService;
+    }
+
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *     path="/api/group",
+     *     summary="Get Group list",
+     *     tags={"Group"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/GroupResource")
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthenticated."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="You don't have permission."
+     *             )
+     *         )
+     *     ),
+     *     security={{"api_key":{}}}
+     * )
      */
     public function index()
     {
-        //
+        if ($this->groupService->checkIsAdminOrMd()) {
+            $groups = $this->groupService->getAllGroup();
+
+            if ($groups) {
+                return GroupResource::collection($groups)->additional([
+                    "status" => "success",
+                ]);
+            }
+
+            return response()->json([
+                "status" => "error",
+                "message" => "No group found",
+            ], 404);
+        }
+
+        return response()->json([
+            "status" => "error",
+            "message" => "You don't have permission",
+        ], 403);
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * @OA\POST(
+     *     path="/api/group/create",
+     *     summary="Add a new Group",
+     *     tags={"Group"},
+     *     @OA\Parameter(
+     *         name="name",
+     *         in="query",
+     *         description="The name of the group.",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Parameter(
+     *         name="group_id",
+     *         in="query",
+     *         description="ID of the Group.",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="status",
+     *                 type="string",
+     *                 example="success"
+     *             ),
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Group created successfully"
+     *             ),
+     *             @OA\Property(
+     *                 property="data",
+     *                 ref="#/components/schemas/GroupResource"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad Request",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="All fields are required."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthenticated."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="You don't have permission."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="An error occurred."
+     *             )
+     *         )
+     *     ),
+     *     security={{"api_key":{}}}
+     * )
      */
     public function store(Request $request)
     {
-        //
+        if ($this->groupService->checkIsAdminOrMd()) {
+            $validator = Validator::make($request->all(), [
+                'name' => ['required', 'string', 'max:255'],
+                'group_id' => ['required', 'string', 'max:255'],
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => "All fields are required",
+                    'data' => $validator->errors()
+                ], 400);
+            }
+
+            $addGroup = $this->groupService->create($request->all());
+
+            if ($addGroup) {
+                return (new GroupResource($addGroup))->additional([
+                    'status' => 'success',
+                    'message' => 'Group created successfully'
+                ]);
+            }
+
+            return response()->json([
+                "status" => "error",
+                "message" => "An error occurred",
+            ], 500);
+        }
+
+        return response()->json([
+            "status" => "error",
+            "message" => "You don't have permission",
+        ], 403);
     }
 
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *     path="/api/group/view/{group}",
+     *     summary="Get a specific Group",
+     *     tags={"Group"},
+     *     @OA\Parameter(
+     *         name="group",
+     *         in="path",
+     *         required=true,
+     *         description="The ID of the group",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/GroupResource")
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthenticated."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="You don't have permission."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Requested Group not Found",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Requested Group not found"
+     *             )
+     *         )
+     *     ),
+     *     security={{"api_key":{}}}
+     * )
      */
-    public function show(Group $group)
+    public function show($group)
     {
-        //
+        if ($this->groupService->checkIsAdminOrMd()) {
+            $getGroup = $this->groupService->getGroupById($group);
+
+            if ($getGroup) {
+                return (new GroupResource($getGroup))->additional([
+                    'status' => 'success'
+                ]);
+            }
+
+            return response()->json([
+                "status" => "error",
+                "message" => "No group found",
+            ], 404);
+        }
+
+        return response()->json([
+            "status" => "error",
+            "message" => "You don't have permission",
+        ], 403);
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * @OA\PUT(
+     *     path="/api/group/update/{group}",
+     *     summary="Update group details",
+     *     tags={"Group"},
+     *     @OA\Parameter(
+     *         name="group",
+     *         in="path",
+     *         description="The ID of the group to update.",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="name",
+     *                 type="string",
+     *                 description="Name of the group",
+     *                 example="Duplex"
+     *             ),
+     *             @OA\Property(
+     *                 property="group_id",
+     *                 type="string",
+     *                 description="ID of the group",
+     *                 example="2"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="status",
+     *                 type="string",
+     *                 example="success"
+     *             ),
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Group updated successfully"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad Request",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="All fields are required."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthenticated."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="You don't have permission."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="An error occurred."
+     *             )
+     *         )
+     *     ),
+     *     security={{"api_key":{}}}
+     * )
      */
-    public function edit(Group $group)
+    public function update(Request $request, $group)
     {
-        //
+        if ($this->groupService->checkIsAdminOrMd()) {
+            $validator = Validator::make($request->all(), [
+                'name' => ['required', 'string', 'max:255'],
+                'group_id' => ['required', 'string', 'max:255'],
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => "All fields are required",
+                    'data' => $validator->errors()
+                ], 400);
+            }
+
+            $updateGroup = $this->groupService->updateGroup($request->all(), $group);
+
+            if ($updateGroup) {
+                return response()->json([
+                    "status" => "success",
+                    "message" => "Group updated successfully",
+                ], 200);
+            }
+
+            return response()->json([
+                "status" => "error",
+                "message" => "An error occurred",
+            ], 500);
+        }
+
+        return response()->json([
+            "status" => "error",
+            "message" => "You don't have permission",
+        ], 403);
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Delete(
+     *     path="/api/group/delete/{group}",
+     *     summary="Delete a specific group",
+     *     tags={"Group"},
+     *     @OA\Parameter(
+     *         name="group",
+     *         in="path",
+     *         required=true,
+     *         description="The ID of the group to delete",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="status",
+     *                 type="string",
+     *                 example="success"
+     *             ),
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Group deleted successfully"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="status",
+     *                 type="string",
+     *                 example="error"
+     *             ),
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="An error occurred"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthenticated."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="You don't have permission"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Requested Group not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Requested Group not found"
+     *             )
+     *         )
+     *     ),
+     *     security={{"api_key":{}}}
+     * )
      */
-    public function update(Request $request, Group $group)
+    public function destroy($group)
     {
-        //
-    }
+        if ($this->groupService->checkIsAdminOrMd()) {
+            $groupDelete = $this->groupService->deleteGroup($group);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Group $group)
-    {
-        //
+            if ($groupDelete) {
+                return response()->json([
+                    "status" => "success",
+                    "message" => "Group deleted successfully",
+                ], 200);
+            }
+
+            return response()->json([
+                "status" => "error",
+                "message" => "An error occurred",
+            ], 400);
+        }
+
+        return response()->json([
+            "status" => "error",
+            "message" => "You don't have permission",
+        ], 403);
     }
 }
