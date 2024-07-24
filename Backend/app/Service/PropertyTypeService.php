de9b6c2dda830c0aaa6fa2eed3770255823755f3
samkaypro <samkaypro@gmail.com>
2024-07-24 10:49:08 +0100

    csf

--- Backend/app/Service/PropertyTypeService.php
+++ Backend/app/Service/PropertyTypeService.php
@@ -13,26 +13,66 @@ use Illuminate\Support\Facades\Validator;
 
 class PropertyTypeService
 {
+    /**
+     * Retrieve all PropretyType.
+     *
+     * This method returns a collection of all PropretyType from the database.
+     *
+     * @return Collection|PropretyType[]  A collection of PropretyType models.
+     */
     public function getAllPropertyType()
     {
         return PropertyType::all();
     }
 
+    /**
+     * Create a new PropretyType.
+     *
+     * This method creates a new PropretyType with the given data.
+     *
+     * @param array $data  The data to create the PropretyType.
+     * @return PropretyType  The created PropretyType model.
+     */
     public function create($data)
     {
         return PropertyType::create($data);
     }
 
+    /**
+     * Retrieve a PropretyType by its name.
+     *
+     * This method returns the first PropretyType that matches the given name.
+     *
+     * @param string $name  The name of the PropretyType.
+     * @return PropretyType|null  The PropretyType model if found, null otherwise.
+     */
     public function getPropertyTypeFromPropertyTypeName($name)
     {
         return PropertyType::where('name', $name)->first();
     }
 
+    /**
+     * Retrieve a PropretyType by its ID.
+     *
+     * This method returns the first PropretyType that matches the given ID.
+     *
+     * @param int $id  The ID of the PropretyType.
+     * @return PropretyType|null  The PropretyType model if found, null otherwise.
+     */
     public function getPropertyTypeById($id)
     {
         return PropertyType::where('id', $id)->first();
     }
 
+    /**
+     * Update a PropretyType by its ID.
+     *
+     * This method updates the PropretyType with the given ID using the provided data.
+     *
+     * @param array $data The data to update the PropretyType.
+     * @param int $id  The ID of the PropretyType to update.
+     * @return bool  True if the update was successful, false otherwise.
+     */
     public function updatePropertyType($data, $id)
     {
         $updateData = $this->getPropertyTypeById($id);
@@ -43,6 +83,14 @@ class PropertyTypeService
         }
     }
 
+    /**
+     * Delete a PropretyType by its ID.
+     *
+     * This method deletes the PropretyType with the given ID.
+     *
+     * @param int $id  The ID of the PropretyType to delete.
+     * @return bool|null  True if the deletion was successful, false otherwise. Null if the PropretyType was not found.
+     */
     public function deletePropertyType($id)
     {
         $delete = $this->getPropertyTypeById($id);
@@ -53,7 +101,14 @@ class PropertyTypeService
         }
     }
 
-
+    /**
+     * Check if the authenticated user is an admin or managing director (MD).
+     *
+     * This method checks if the currently authenticated user has a role ID
+     * that corresponds to either an admin or managing director.
+     *
+     * @return bool  True if the user is an admin or MD, false otherwise.
+     */
     public function checkIsAdminOrMd()
     {
         if (Auth::user()->role_id == User::ROLE_ADMIN || Auth::user()->role_id == User::ROLE_MD) {
